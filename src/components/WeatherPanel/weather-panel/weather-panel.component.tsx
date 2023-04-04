757f98e390551c7baa918078f9c06f9cc5c493f1
wilsonsantos93 <wilsonjesus93@gmail.com>
2023-04-04 21:48:04 +0100

    added new file and folder

---
+++ src/components/WeatherPanel/weather-panel/weather-panel.component.tsx
@@ -0,0 +1,19 @@
+import { Stack } from "react-bootstrap";
+import WeatherInfoSelector from "../weather-info-selector/weather-info-selector.component";
+import WeatherDateSelector from "../weather-date-selector/weather-date-selector.component";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './weather-panel.styles.css';
+
+const WeatherPanel = () => {
+    return (
+        <div className="weather-panel">
+            <Stack>
+                <WeatherInfoSelector/>
+                <WeatherDateSelector/>
+            </Stack>
+        </div>
+    );
+}
+
+export default WeatherPanel;
+
